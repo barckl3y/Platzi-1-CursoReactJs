a4f7ff1d4fb0110d4d3d44d584e8221ce0754ba3
barckl3y <barckl3y@MacBook-Pro-de-barckl3y.local>
2023-08-30 21:01:40 -0500

    Cambios realizados en el video 2 del curso

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './platzi.webp';
 import './App.css';
 
+// Un componente en React empieza por convención con mayuscula
+// Lo de adentro de return es JSX no es HTMl , en JSX se pone className en ves de class que se pone en HTML
+// header, img, p , son elementos de REACT , si empezaran con  mayuscula fueran componentes de REACT
 function App() {
   return (
     <div className="App">
+
+      <TodoItem mensaje='tarea 1' />
+      <TodoItem mensaje='tarea 2' />
+      <TodoItem mensaje='tarea 3' />
+      <TodoItem mensaje='tarea 5' />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,10 +24,20 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
+  );
+}
+
+function TodoItem(props){
+  return(
+    <li>
+      <span>V</span>
+      <p>{props.mensaje}</p>
+      <span>X</span>
+    </li>
   );
 }
 
