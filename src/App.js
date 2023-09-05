beebe2afe56c07526dfe393e9284bb872dd8552b
barckl3y <barckl3y@MacBook-Pro-de-barckl3y.local>
2023-09-05 10:59:34 -0500

    Cambios realizados en el video 13 del curso

--- src/App.js
+++ src/App.js
@@ -5,24 +5,44 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
-const defaultTodos=[
-  {id:1,text : 'Cortar cebolla',completed:true},
-  {id:2,text : 'Tomar el curso de intro a React.js',completed:false},
-  {id:3,text : 'Llorar con la Llorona',completed:false},
-  {id:4,text : 'LALALALA',completed:false},
-  {id:5,text : 'Conseguir empleo remoto',completed:true},
-  {id:6,text : 'Usar estados derivados',completed:true},
-  {id:7,text : 'Probando tildes como canción',completed:true},
-];
+// const defaultTodos=[
+//   {id:1,text : 'Cortar cebolla',completed:true},
+//   {id:2,text : 'Tomar el curso de intro a React.js',completed:false},
+//   {id:3,text : 'Llorar con la Llorona',completed:false},
+//   {id:4,text : 'LALALALA',completed:false},
+//   {id:5,text : 'Conseguir empleo remoto',completed:true},
+//   {id:6,text : 'Usar estados derivados',completed:true},
+//   {id:7,text : 'Probando tildes como canción',completed:true},
+// ];
+
+// localStorage.setItem('TODOS_V1',JSON.stringify(defaultTodos));
+
+// const stringfiedTodos=JSON.stringify(defaultTodos);
+// localStorage.setItem('TODOS_V1',stringfiedTodos);
+// const localStorageTodos=localStorage.getItem('TODOS_V1');
+// const parsedTodos=JSON.parse(localStorageTodos);
+// localStorage.removeItem('TODOS_V1');
 
 // Un componente en React empieza por convención con mayuscula
 // Lo de adentro de return es JSX no es HTMl , en JSX se pone className en ves de class que se pone en HTML
 // header, img, p , son elementos de REACT , si empezaran con  mayuscula fueran componentes de REACT
 function App() {
+  // usando localstorage , recordar los pasos para guardar objetos
+  // en localstoragre
+
+  const localStorageTodos=localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos){
+    localStorage.setItem('TODOS_V1',JSON.stringify([]));
+    parsedTodos=[];
+  }else{
+    parsedTodos=JSON.parse(localStorageTodos);
+  }
 
   const [searchValue,setSearchValue]=React.useState('');
-
-  const [todos,setTodos]=React.useState(defaultTodos);
+  const [todos,setTodos]=React.useState(parsedTodos);
+  // const [todos,setTodos]=React.useState(defaultTodos);
   const completedTodos=todos.filter(todo => todo.completed).length; //estos son estados derivados , creados a partir de un estado use state
   const totalTodos=todos.length;//estos son estados derivados , creados a partir de un estado use state
 
@@ -32,11 +52,17 @@ function App() {
 
   // console.log('Los usuarios buscan todos de ' +searchValue);
 
+  const saveTodos = (newTodos) =>{
+    localStorage.setItem('TODOS_V1',JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
   const deleteTodo =(id) => {
     // con los ... se dice quiero una copia de esa variable
 
     const newTodos = todos.filter(todo => todo.id !== id);
-    setTodos(newTodos);
+    // setTodos(newTodos);
+    saveTodos(newTodos);
 
   };
 
@@ -49,8 +75,8 @@ function App() {
     );
     // splice dice desde que index , a partir de ahí cuantos pedazos corto
     newTodos.splice(todoIndex,1);
-    setTodos(newTodos);
-
+    // setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const completedTodo =(id) => {
@@ -62,7 +88,8 @@ function App() {
 
     // con esto ponemos el valor contrario del estado actual , completado o no completado al dar clic en los items
     newTodos[todoIndex].completed=!newTodos[todoIndex].completed;
-    setTodos(newTodos);
+    // setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
